56a94fa58a07e0356fc0a9360199a96de8be8e00
tkmagesh <tkmagesh77@gmail.com>
2015-02-10 12:50:26 +0530

    Closures

---
+++ Day-02/01-Closures/primeFinder.js
@@ -0,0 +1,15 @@
+function getPrimeFinder(){
+    var cache = {};
+    function checkPrime(n){
+        console.log("Checking prime..");
+        if (n <= 3) return true;
+        for(var i=2; i <= (n/2); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+    return function(n){
+        if (typeof cache[n] === "undefined")
+           cache[n] = checkPrime(n);
+        return cache[n];
+    }
+}
